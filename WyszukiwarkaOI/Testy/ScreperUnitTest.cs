d7560d72b8ee8b3c3814cb3eb74c8d0ba5bfa38a
augustus322 <127135490+augustus322@users.noreply.github.com>
2024-03-18 19:34:15 +0100

    Connection test

---
+++ WyszukiwarkaOI/Testy/ScreperUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using WyszukiwarkaOI_webScraper;
+
+namespace Testy
+{
+    public class ScraperUnitTest
+    {
+        WebScraper scraper = new();
+
+        [Fact]
+        public void ConnectionTest()
+        {
+            string url = "https://www.okazje.info.pl/search/?q=rower";
+            var t1 = scraper.GetWebsiteHtmlAsync(url).Result.isSuccess;
+            Assert.True(t1);
+        }
+
+        public void a()
+        {
+
+        }
+    }
+}
