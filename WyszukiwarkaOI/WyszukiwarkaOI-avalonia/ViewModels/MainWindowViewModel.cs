7ab6fad5ae1008d56b90e8b69ae6cddb07c34562
dawid200272 <45143323+dawid200272@users.noreply.github.com>
2024-03-18 19:30:15 +0100

    Repo clean up
    Deleted avalonia project.
    Deleted WyszukiwarkaOI-WPF project.
    Dleted wpf.txt file.

--- WyszukiwarkaOI/WyszukiwarkaOI-avalonia/ViewModels/MainWindowViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ViewModels
-{
-    public class MainWindowViewModel : ViewModelBase
-    {
-        public string Greeting => "Welcome to Avalonia!";
-    }
-}
--- WyszukiwarkaOI/WyszukiwarkaOI-avalonia/ViewModels/ViewModelBase.cs
+++
@@ -1,8 +0,0 @@
-﻿using ReactiveUI;
-
-namespace ViewModels
-{
-    public class ViewModelBase : ReactiveObject
-    {
-    }
-}
