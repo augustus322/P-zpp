fca66af9ac0e72186a8cbae8f7fa9c67d0c9d694
dawid200272 <45143323+dawid200272@users.noreply.github.com>
2024-03-18 19:09:21 +0100

    Project swap
    Added WPF project and deleted old one (that on .NET Framework).
    Added EF project and deleted old one.

---
+++ WyszukiwarkaOI/WyszukiwarkaOI.EntityFramework/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WyszukiwarkaOI.EntityFramework;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+	public DbSet<Product> Products { get; set; }
+}
+
+// TODO: Move that class to some kind of 'Models' folder in 'Domain' project or sth
+public class Product(string shopLink, string name, decimal price, string shopName, string? description = null)
+{
+	public int Id { get; set; }
+	public string Name { get; set; } = name;
+	public string? Description { get; set; } = description;
+	public decimal Price { get; set; } = price;
+	public string ShopName { get; set; } = shopName;
+	public string ShopLink { get; set; } = shopLink;
+}
---
+++ WyszukiwarkaOI/WyszukiwarkaOI.EntityFramework/AppDbContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WyszukiwarkaOI.EntityFramework;
+public class AppDbContextFactory(string dbConnectionString)
+{
+	private readonly string _dbConnectionString = dbConnectionString;
+
+	public AppDbContext CreateDbContext()
+	{
+		var options = new DbContextOptionsBuilder<AppDbContext>();
+		options.UseSqlite(_dbConnectionString);
+
+		return new AppDbContext(options.Options);
+	}
+}
