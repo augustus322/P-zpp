8614ad6a66f2bad69db2bc8d231b82fb596da76d
dawid200272 <45143323+dawid200272@users.noreply.github.com>
2024-03-18 19:09:21 +0100

    Project swap
    Added WPF project and deleted old one (that on .NET Framework).
    Added EF project and deleted old one.

---
+++ WyszukiwarkaOI/WyszukiwarkaOI.WPF/App.xaml.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+using WyszukiwarkaOI.EntityFramework;
+using WyszukiwarkaOI_webScraper;
+
+namespace WyszukiwarkaOI.WPF;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+	private readonly IHost _host;
+
+    public App()
+    {
+        _host = CreateHostBuilder().Build();
+    }
+
+	public static IHostBuilder CreateHostBuilder(string[]? args = null)
+	{
+		return Host.CreateDefaultBuilder(args)
+			.ConfigureAppConfiguration(config =>
+			{
+				config.AddJsonFile("appsettings.json");
+			})
+			.ConfigureServices((context, services) =>
+			{
+				string connectionString = context.Configuration.GetConnectionString("Default")!;
+
+				services.AddDbContext<AppDbContext>(options => options.UseSqlite(connectionString));
+				services.AddSingleton(new AppDbContextFactory(connectionString));
+			});
+	}
+
+	protected override async void OnStartup(StartupEventArgs e)
+	{
+		_host.Start();
+
+		string baseAddress = "http://www.okazje.info.pl/";
+		string searchPhrase = "rower"; // get that value from search bar
+
+		string url = $"{baseAddress}search/?q={searchPhrase}";
+
+		WebScraper webScraper = new WebScraper();
+
+		(string? html, bool isSuccess) = await webScraper.GetWebsiteHtmlAsync(url);
+
+		if (!isSuccess)
+		{
+			return;
+		}
+
+		var result = await webScraper.GetChildrenOfGivenElementAsync(".productsBox", html!);
+
+		if (!result.isSuccess)
+		{
+			return;
+		}
+
+		IEnumerable<AngleSharp.Dom.IElement> products = result.children!;
+
+		Func<string, string, decimal, string, string?, Product> ctor = (p1, p2, p3, p4, p5) => new Product(p1, p2, p3, p4, p5);
+
+		var scraperResult = webScraper.GetElementsInfo(products, ctor);
+
+		if (!scraperResult.isSuccess)
+		{
+			return;
+		}
+
+		// add products info to database
+
+		Console.ReadLine();
+
+
+		base.OnStartup(e);
+	}
+
+	protected override async void OnExit(ExitEventArgs e)
+	{
+		await _host.StopAsync();
+		_host.Dispose();
+
+		base.OnExit(e);
+	}
+}
---
+++ WyszukiwarkaOI/WyszukiwarkaOI.WPF/Stores/ProductStore.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WyszukiwarkaOI.EntityFramework;
+
+namespace WyszukiwarkaOI.WPF.Stores;
+public class ProductStore : IEnumerable<Product>
+{
+	// Lista do przechowywania produktów w pamięci (wewnętrzna)
+	private readonly List<Product> _products = new List<Product>();
+
+	// Publicznie dostępna kolekcja produktów
+	public IEnumerable<Product> Products => _products;
+
+	#region CRUD Operations
+
+	public void Add(Product product)
+	{
+		_products.Add(product);
+	}
+
+	public void AddRange(IEnumerable<Product> products)
+	{
+		_products.AddRange(products);
+	}
+
+	#endregion
+
+	public IEnumerator<Product> GetEnumerator()
+	{
+		return _products.GetEnumerator();
+	}
+
+	IEnumerator IEnumerable.GetEnumerator()
+	{
+		return GetEnumerator();
+	}
+}
